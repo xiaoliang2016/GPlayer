f767fcd232f43f050981c3dfbf75535de6fc5034
Krzysztof Gawrys <kgawrys@concisesoftware.com>
2015-11-12 12:48:28 +0100

    copying info removed more plugins

--- jni/Android.mk
+++ jni/Android.mk
@@ -29,12 +29,12 @@ GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build
 
 
 GSTREAMER_PLUGINS_CODECS_RESTRICTED := mad faad
-GSTREAMER_PLUGINS_CORE := coreelements app audioconvert audiorate audioresample typefindfunctions volume autodetect
+GSTREAMER_PLUGINS_CORE := coreelements audioconvert audioresample typefindfunctions volume autodetect
 GSTREAMER_PLUGINS_SYS := opensles
 GSTREAMER_PLUGINS_PLAYBACK := playback
-GSTREAMER_PLUGINS_EFFECTS := audiofx interleave level multifile replaygain autoconvert inter liveadder rawparse segmentclip speed audiomixer
-GSTREAMER_PLUGINS_CODECS := subparse ogg vorbis apetag audioparsers auparse flv flac icydemux id3demux isomp4 multipart taglib wavparse fragmented id3tag gdp 
-GSTREAMER_PLUGINS_NET := tcp rtsp rtp rtpmanager soup udp dataurisrc sdp srtp
+GSTREAMER_PLUGINS_EFFECTS := 
+GSTREAMER_PLUGINS_CODECS := ogg vorbis audioparsers flac icydemux id3demux isomp4 wavparse fragmented id3tag 
+GSTREAMER_PLUGINS_NET := tcp soup
 GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_PLAYBACK) $(GSTREAMER_PLUGINS_EFFECTS) $(GSTREAMER_PLUGINS_NET) $(GSTREAMER_PLUGINS_SYS) $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_CODECS_RESTRICTED)
 G_IO_MODULES              := gnutls
 
